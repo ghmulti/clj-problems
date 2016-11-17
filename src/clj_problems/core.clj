e0db8ba5380883d2079b483503b835889665d639
multi <ghmulti@gmail.com>
2016-11-17 21:14:28 +0100

    cleanup

--- src/clj_problems/core.clj
+++ src/clj_problems/core.clj
@@ -13,12 +13,12 @@
 (defn problem166 [mfn & args]
   (if (apply mfn args) :lt (if (apply mfn (reverse args)) :gt :eq)))
 
-(defn run-166-tests [mfn]
-  (and
-    (= :gt (mfn < 5 1))
-    (= :eq (mfn (fn [x y] (< (count x) (count y))) "pear" "plum"))
-    (= :lt (mfn (fn [x y] (< (mod x 5) (mod y 5))) 21 3))
-    (= :gt (mfn > 0 2))))
+(defn run-166-tests []
+  [
+   (= :gt (problem166 < 5 1))
+   (= :eq (problem166 (fn [x y] (< (count x) (count y))) "pear" "plum"))
+   (= :lt (problem166 (fn [x y] (< (mod x 5) (mod y 5))) 21 3))
+   (= :gt (problem166 > 0 2))])
 
 ;;
 
@@ -35,26 +35,26 @@
   ([f m n] (build-lazy-columns #(build-lazy-row f % n) m))
   ([f m n s t] (take s (map #(take t %) (problem168 f m n)))))
 
-(def result-168-fn
-  (let [build-lazy-row (fn build-lazy-row [f l r]
-                         (lazy-seq (cons (f l r) (build-lazy-row f l (inc r)))))
-        build-lazy-columns (fn build-lazy-columns [row-builder index]
-                             (lazy-seq (cons (row-builder index) (build-lazy-columns row-builder (inc index)))))]
-    (fn problem168
-      ([f] (build-lazy-columns #(build-lazy-row f % 0) 0))
-      ([f m n] (build-lazy-columns #(build-lazy-row f % n) m))
-      ([f m n s t] (take s (map #(take t %) (problem168 f m n)))))))
+;(def result-168-fn
+;  (let [build-lazy-row (fn build-lazy-row [f l r]
+;                         (lazy-seq (cons (f l r) (build-lazy-row f l (inc r)))))
+;        build-lazy-columns (fn build-lazy-columns [row-builder index]
+;                             (lazy-seq (cons (row-builder index) (build-lazy-columns row-builder (inc index)))))]
+;    (fn problem168
+;      ([f] (build-lazy-columns #(build-lazy-row f % 0) 0))
+;      ([f m n] (build-lazy-columns #(build-lazy-row f % n) m))
+;      ([f m n s t] (take s (map #(take t %) (problem168 f m n)))))))
 
-(defn run-168-tests [mfn]
+(defn run-168-tests []
   [
-   (= (take 5 (map #(take 6 %) (mfn str)))
+   (= (take 5 (map #(take 6 %) (problem168 str)))
       [["00" "01" "02" "03" "04" "05"]
        ["10" "11" "12" "13" "14" "15"]
        ["20" "21" "22" "23" "24" "25"]
        ["30" "31" "32" "33" "34" "35"]
        ["40" "41" "42" "43" "44" "45"]])
 
-   (= (take 6 (map #(take 5 %) (mfn str 3 2)))
+   (= (take 6 (map #(take 5 %) (problem168 str 3 2)))
       [["32" "33" "34" "35" "36"]
        ["42" "43" "44" "45" "46"]
        ["52" "53" "54" "55" "56"]
@@ -62,7 +62,7 @@
        ["72" "73" "74" "75" "76"]
        ["82" "83" "84" "85" "86"]])
 
-   (= (mfn * 3 5 5 7)
+   (= (problem168 * 3 5 5 7)
       [[15 18 21 24 27 30 33]
        [20 24 28 32 36 40 44]
        [25 30 35 40 45 50 55]
