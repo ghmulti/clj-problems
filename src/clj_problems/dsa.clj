13a860639555c13b5bbb8d78968e4fd55824ca0f
multi <ghmulti@gmail.com>
2016-12-07 23:39:16 +0100

    3.2 divide and conquer - calculate number of inversions in collection

--- src/clj_problems/dsa.clj
+++ src/clj_problems/dsa.clj
@@ -1,16 +1,19 @@
 (ns clj-problems.dsa
   (:refer-clojure))
 
+(defn get-inversions [{inv :inversions}] (if inv inv 0))
+
 (defn- merge-cols [col1 col2]
   ;(println "Merging" col1 col2)
   (loop [[fl & rl :as l] col1
          [fr & rr :as r] col2
-         un []]
+         un (with-meta [] {:inversions (+ (-> col1 meta get-inversions) (-> col2 meta get-inversions))})]
     ;(println "Iterate " fl fr un)
     (if (or fl fr)
       (if (or (and fl fr (< fl fr)) (not fr))
         (recur rl r (conj un fl))
-        (recur l rr (conj un fr)))
+        (recur l rr (-> (conj un fr)
+                        (with-meta {:inversions (-> un meta get-inversions (+ (count l)))} ))))
       un)))
 
 (defn merge-sort [col]
@@ -18,3 +21,9 @@
     (if (= 1 sz)
       col
       (merge-cols (merge-sort (take (/ sz 2) col)) (merge-sort (drop (/ sz 2) col))))))
+
+
+(defn number-of-inversions [col]
+  (-> (merge-sort col)
+      meta
+      get-inversions))
